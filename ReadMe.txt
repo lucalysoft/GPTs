727911a2af596c633356021d2a2610b0620e16fc
lucalysoft <lucaly17@gmail.com>
2023-03-10 13:12:22 -0800

    ok

--- ReadMe.txt
+++ ReadMe.txt
@@ -10,11 +10,6 @@
 
 
 
-I 'd like to chat in here.
-https://app.slack.com/client/T04SC5U0PG9/D04SXBJNS2K
-In chatting, I feel low speed.
-
-
 
 
 
---
+++ lookhere.js
@@ -0,0 +1,5 @@
+
+// I 'd like to chat in here.
+// https://app.slack.com/client/T04SC5U0PG9/D04SXBJNS2K
+// In chatting, I feel low speed in freelancer website.
+
