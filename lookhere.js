7cc2b6afef0d3db32e3aadcda0d4f015dd97fada
lucalysoft <lucaly17@gmail.com>
2023-03-10 13:17:34 -0800

    asdfa

--- lookhere.js
+++ lookhere.js
@@ -1,5 +1,9 @@
-
+// Now here state is not good.
 // I 'd like to chat in here.
-// https://app.slack.com/client/T04SC5U0PG9/D04SXBJNS2K
+// I cah invite you in slack. lucaly17@gmail.com
 // In chatting, I feel low speed in freelancer website.
+// Thank you for your Time.
+
+//skype    live:.cid.9a654b79ec08a141
+//discord  lucaly#9244
 
